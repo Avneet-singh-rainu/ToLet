c5527304f740f7e111596d78c31c313fc0d0e22c
fast_Keys <suvrt082003@gmail.com>
2025-04-08 16:48:07 +0530

    added create room component and page

--- To-Let.Client/src/App.jsx
+++ To-Let.Client/src/App.jsx
@@ -6,6 +6,8 @@ import NoNavLayout from "./layouts/NoNavLayout";
 import RoomDetailsPage from "./pages/RoomDetailsPage";
 import SearchPage from "./pages/SearchPage";
 import Error from "./pages/Error";
+import CreateRoom from "./pages/CreateRoom";
+    
 
 function App() {
    return (
@@ -15,6 +17,7 @@ function App() {
                <Route index element={<Home />} />
                <Route path="room-details/:id" element={<RoomDetailsPage />} />
                <Route path="/search" element={<SearchPage />} />
+               <Route path="/createroom" element={<CreateRoom />} />
                <Route
                   path="/*"
                   element={<Error message={"Page Under Progress..."} />}
---
+++ To-Let.Client/src/pages/CreateRoom.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CreateRoomForm from '../components/Form/CreateRoomForm'
+
+const CreateRoom = () => {
+  return (
+<div className='min-w-screen'>
+<CreateRoomForm/>
+</div>
+      
+  
+  )
+}
+
+export default CreateRoom
